b67916b64afdce5fa4f2ff63f49f623b9bd4384c
fengshanggui <1500081564@qq.com>
2022-03-02 01:21:59 +0800

    PHP后端提交

--- PHPapi/newPer.php
+++ PHPapi/newPer.php
@@ -5,16 +5,19 @@ header("Content-type:text/html; charset=utf-8");
 header('Access-Control-Allow-Origin: *');
 $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
-// $perName=$_POST['perName'];
-// $age=$_POST['age'];
-// $address=$_POST['address'];
-// $perState=$_POST['perState'];
-// $gender=$_POST['gender'];
-$perId=$_GET['perId'];
-// $part=$_POST['part'];
-$sql ="INSERT INTO `personnel`(`员工编号`)VALUES ('{$perId}')";
-
-
+// 解决前端的问题
+$v=(file_get_contents("php://input"));
+var_dump(json_decode($v,true));
+$c=json_decode($v,true);
+ $perId=$c['perId'];
+$perName=$c['perName'];
+$gender=$c['gender'];
+$age=$c['age'];
+$part=$c['part'];
+$address=$c['address'];
+$contact=$c['contact'];
+$perState=$c['perState'];
+$sql ="INSERT INTO `personnel`(`员工编号`, `姓名`, `性别`, `年龄`, `部门`, `地址`, `联系方式`, `在职状态`) VALUES('{$perId}','{$perName}','{$gender}','{$age}','{$part}','{$address}','{$contact}','{$perState}')";
 
 if ($mysqli -> connect_error) {
        die("连接错误：".$mysqli -> connect_error);
@@ -24,7 +27,7 @@ $mysqli -> set_charset(DB_CHARSET);
 // 连接数据库并执行
 $result=$mysqli ->query($sql);
 
-
+echo $age,$address,$contact,$perId,$perName,$perState,$part,$gender;
  //创建一个数组去接收
 // while ($row = $result -> fetch_assoc()) {
 //     $rows[] = $row;   //$rows中保存personnel表中所有记录
@@ -36,4 +39,6 @@ $result=$mysqli ->query($sql);
 // 该函数用于将json文本转换为相应的PHP数据结构。
 // 输出接口
 // echo json_encode($rows);
+return "1";
+ return $perId;
 // return json_encode($rows);
