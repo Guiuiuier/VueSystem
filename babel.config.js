d4af95efcc8a9f4a271b3789575f83f8dbcf853d
Fengshanggui <50706042+Guiuiuier@users.noreply.github.com>
2022-04-07 23:46:25 +0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,11 @@ const prodPlugins=[]
 if(process.env.NODE_ENV==='production'){
   prodPlugins.push('transform-remove-console');
 }
-module.exports = {
+module.exports = {1
   presets: ['@vue/cli-plugin-babel/preset'],
   plugins:[
       ...prodPlugins,
       // 'transform-remove-console'  //在这里直接写会所有环境都移除
       // 发布产品时候的插件数组
     ]
-}
+}1
